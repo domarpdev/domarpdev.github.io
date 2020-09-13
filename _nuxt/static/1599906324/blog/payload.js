9fec1d276f5479b743527c9c28521f50bd11712f
Devireddy Pramod Reddy <devireddy.pramod@gmail.com>
2020-09-13 19:55:06 +0530

    Updated blog like medium & added read time

--- _nuxt/static/1599906324/blog/payload.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("/blog", {data:[{posts:[{title:"My first Blog Post",description:"Learning how to use @nuxt\u002Fcontent to create a blog",img:"first-blog-post.jpg",author:{name:"Pramod",bio:"All about Pramod Devireddy"},slug:"blog1"}]}],fetch:[],mutations:[["SET_APP_BAR_COLOR","#FFF"],["SET_APP_TITLE_COLOR","#000"]]});
