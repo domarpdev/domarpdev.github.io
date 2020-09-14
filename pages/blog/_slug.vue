d3498501c04bad585b95c5721acb6999b480ed13
Devireddy Pramod Reddy <devireddy.pramod@gmail.com>
2020-09-15 00:07:27 +0530

    Added social share links to blog

--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -2,22 +2,32 @@
   <v-container>
     <v-row justify="center">
       <v-col lg="5" md="8" sm="10">
-        <h1 class="blog-title">{{post.title}}</h1>
+        <h1 class="blog-title">{{ post.title }}</h1>
 
-        <h4 class="blog-description">{{post.description}}</h4>
+        <h4 class="blog-description">{{ post.description }}</h4>
 
-        <div class="d-flex blog-details">
-          <div>
-            <v-avatar class="elevation-10" size="55">
-              <img src="pramod-devireddy-96x96.jpg" alt="Pramod Devireddy" />
-            </v-avatar>
-          </div>
-          <div class="ml-3 mt-2">
+        <div class="d-flex flex-wrap blog-details">
+          <v-avatar class="elevation-10" size="55">
+            <img src="pramod-devireddy-96x96.jpg" alt="Pramod Devireddy" />
+          </v-avatar>
+          <div class="ml-3 mt-1">
             <div class="blog-author">Pramod Devireddy</div>
             <div class="blog-time">
               <span>{{ formatDate(post.updatedAt) }}</span> •
               <ReadTime :content="post"></ReadTime>
             </div>
+          </div>
+          <v-spacer></v-spacer>
+          <div class="ml-16 mt-3">
+            <v-btn icon color="#757575" target="_blank" @click="tweet()">
+              <v-icon>mdi-twitter</v-icon>
+            </v-btn>
+            <v-btn class="mx-n1" icon color="#757575" target="_blank" @click="linkedInPost()">
+              <v-icon>mdi-linkedin</v-icon>
+            </v-btn>
+            <v-btn icon color="#757575" target="_blank" @click="facebookPost()">
+              <v-icon>mdi-facebook</v-icon>
+            </v-btn>
           </div>
         </div>
 
@@ -47,6 +57,68 @@ export default {
     formatDate(date) {
       const options = { year: "numeric", month: "long", day: "numeric" };
       return new Date(date).toLocaleDateString("en", options);
+    },
+
+    tweet() {
+      var tweetURL =
+        "https://twitter.com/intent/tweet?text=" +
+        document.title +
+        "&url=" +
+        location.href;
+      window.open(tweetURL);
+    },
+
+    linkedInPost() {
+      var linkedInPostURL =
+        "https://www.linkedin.com/shareArticle/?mini=true&url=" + location.href;
+      this.PopupCenter(linkedInPostURL, 500, 500);
+    },
+
+    facebookPost() {
+      var fbPostURL =
+        "https://www.facebook.com/sharer/sharer.php?u=" +
+        location.href +
+        "&display=page";
+      window.open(fbPostURL);
+    },
+
+    PopupCenter(url, w, h) {
+      // Fixes dual-screen position                         Most browsers      Firefox
+      var dualScreenLeft =
+        window.screenLeft != undefined ? window.screenLeft : screen.left;
+      var dualScreenTop =
+        window.screenTop != undefined ? window.screenTop : screen.top;
+
+      var width = window.innerWidth
+        ? window.innerWidth
+        : document.documentElement.clientWidth
+        ? document.documentElement.clientWidth
+        : screen.width;
+      var height = window.innerHeight
+        ? window.innerHeight
+        : document.documentElement.clientHeight
+        ? document.documentElement.clientHeight
+        : screen.height;
+
+      var left = width / 2 - w / 2 + dualScreenLeft;
+      var top = height / 2 - h / 2 + dualScreenTop;
+      var newWindow = window.open(
+        url,
+        "_blank",
+        "scrollbars=yes, width=" +
+          w +
+          ", height=" +
+          h +
+          ", top=" +
+          top +
+          ", left=" +
+          left
+      );
+
+      // Puts focus on the newWindow
+      if (window.focus) {
+        newWindow.focus();
+      }
     },
   },
 
@@ -92,7 +164,6 @@ export default {
 };
 </script>
 
-
 <style>
 .blog-title {
   font-size: 48px;
@@ -132,9 +203,9 @@ export default {
 
 .nuxt-content p {
   font-size: 16px;
-  margin-bottom: 20px;
-  word-spacing: 3px;
-  line-height: 28px;
+  margin-bottom: 30px;
+  word-spacing: 2px;
+  line-height: 32px;
 }
 
 .v-application code {
