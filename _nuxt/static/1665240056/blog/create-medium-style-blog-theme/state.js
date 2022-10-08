98e014340897a30b865eb1c3f1cba9369502597f
Devireddy Pramod Reddy <devireddy.pramod@gmail.com>
2022-10-08 20:11:45 +0530

    updated blog post styles

---
+++ _nuxt/static/1665240056/blog/create-medium-style-blog-theme/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1665240056",layout:"default",error:a,state:{appBarColor:"#FFF",appTitleColor:"#000"},serverRendered:true,routePath:"\u002Fblog\u002Fcreate-medium-style-blog-theme",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"c2e19f79"}}}}(null));
---
+++ _nuxt/static/1665240056/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1665240056",layout:"default",error:a,state:{appBarColor:"#4C8BF4",appTitleColor:"#FFF"},serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"c2e19f79"}}}}(null,"\u002F"));
