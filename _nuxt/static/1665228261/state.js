87ee31fcd6379783ed65cf13291efd3c9a996b8a
Devireddy Pramod Reddy <devireddy.pramod@gmail.com>
2022-10-08 17:05:49 +0530

    updated profile

---
+++ _nuxt/static/1665228261/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1665228261",layout:"default",error:a,state:{appBarColor:"#4C8BF4",appTitleColor:"#FFF"},serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"571acd5a"}}}}(null,"\u002F"));
