35814ef726d8b2476bbda8f5f7d18d60e3fccc0a
Devireddy Pramod Reddy <devireddy.pramod@gmail.com>
2024-01-27 10:48:10 +0530

    updated

---
+++ _nuxt/static/1706331678/blog/create-medium-style-blog-theme/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1706331678",layout:"default",error:a,state:{appBarColor:"#FFF",appTitleColor:"#000"},serverRendered:true,routePath:"\u002Fblog\u002Fcreate-medium-style-blog-theme",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"6f730ebd"}}}}(null));
---
+++ _nuxt/static/1706331678/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1706331678",layout:"default",error:a,state:{appBarColor:"#4C8BF4",appTitleColor:"#FFF"},serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"6f730ebd"}}}}(null,"\u002F"));
